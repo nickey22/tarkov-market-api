50ee53e8f7d594fe90d8241ec4c8ffff10592623
Nickey Gertsberg <gertsberg@hotmail.com>
2020-03-08 20:01:16 -0400

    heroku

--- server/app.js
+++ server/app.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = process.env.port || 8080;
 const axios = require("axios");
+const path = require("path");
 app.use(express.urlencoded({extended:true}));
 app.use(express.json({limit:"50mb"}));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
+console.log(`PORT: ${PORT}`);
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+})
 
 app.get('/api/test',async (req,res)=>{
     const tarkovAPI = "https://eft-loot.com/page-data/index/page-data.json";
